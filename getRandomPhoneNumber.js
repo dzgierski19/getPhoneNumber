ea4f1899011f3def0f7e01addc20bb162a8728b4
dzgierski19 <dzgierskigit@gmail.com>
2023-03-24 21:16:35 +0100

    console.log deleted

--- getRandomPhoneNumber.js
+++ getRandomPhoneNumber.js
@@ -3,7 +3,6 @@ const getRandomNineDigits = () => {
   for (let i = 0; i < 9; i++) {
     randomNineDigits += Math.floor(Math.random() * 10);
   }
-  console.log(randomNineDigits);
   if (randomNineDigits[0] === "0") {
     return getRandomNineDigits();
   }
